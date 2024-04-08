7962e4974525fe0183ee66496e95138e331c46bf
SawanPanwar <skpanwar25@gmail.com>
2024-04-08 12:27:30 +0530

    done

---
+++ core-java-12/src/com/rays/date/TestDate.java
@@ -0,0 +1,13 @@
+package com.rays.date;
+
+import java.util.Date;
+
+public class TestDate {
+
+	public static void main(String[] args) {
+
+		Date today = new Date();
+
+		System.out.println(today);
+	}
+}
---
+++ core-java-12/src/com/rays/method/TestExercise.java
@@ -0,0 +1,13 @@
+package com.rays.method;
+
+public class TestExercise {
+	
+	public static void main(String[] args) {
+		
+		Exercise ex = new Exercise();
+		
+		ex.armstrongNumber(154);
+		
+	}
+
+}
