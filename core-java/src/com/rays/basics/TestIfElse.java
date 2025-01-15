bb38146aee2e9269ebc3e834aa38bbce988524ea
SawanPanwar <skpanwar25@gmail.com>
2025-01-15 12:00:14 +0530

    15-01-2025

---
+++ core-java/src/com/rays/basics/TestIfElse.java
@@ -0,0 +1,25 @@
+package com.rays.basics;
+
+public class TestIfElse {
+
+	public static void main(String[] args) {
+
+		int a = 100;
+
+		int b = 1001;
+
+		if (a > b) {
+
+			System.out.println("a is greater");
+
+		} else if (a == b) {
+
+			System.out.println("a equal to b");
+
+		} else {
+
+			System.out.println("b is greater");
+
+		}
+	}
+}
