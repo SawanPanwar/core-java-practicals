6bfc49ead9231454b60be209f8b34326d606f93d
SawanPanwar <skpanwar25@gmail.com>
2025-01-22 16:04:35 +0530

    22-01-2025;

---
+++ core-java/src/com/rays/string/TestCharCheck.java
@@ -0,0 +1,22 @@
+package com.rays.string;
+
+public class TestCharCheck {
+
+	public static void main(String[] args) {
+
+		String name = "vijay dinanath chouhan";
+
+		char ch = 'a';
+
+		int count = 0;
+
+		for (int i = 0; i < name.length(); i++) {
+			
+			if (name.charAt(i) == ch) {
+				count++;
+			}
+		}
+
+		System.out.println("count = " + count);
+	}
+}
