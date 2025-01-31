bb883ad0caa79871b33d90ab47b856405825d706
SawanPanwar <skpanwar25@gmail.com>
2025-01-31 12:11:28 +0530

    31-01-2025

---
+++ core-java/src/com/rays/oop/TestShape.java
@@ -0,0 +1,17 @@
+package com.rays.oop;
+
+public class TestShape {
+
+	public static void main(String[] args) {
+
+		Shape s = new Shape();
+
+		s.setColor("red");
+		s.setBorderWidth(100);
+
+		System.out.println(s.getColor());
+
+		System.out.println(s.getBorderWidth());
+
+	}
+}
